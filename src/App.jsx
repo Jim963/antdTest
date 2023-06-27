ebf063d65ce5409484190049cdfff5c119dbbf9c
jim.liu <jim.liu@kangdainfo.com>
2023-06-27 14:38:30 +0800

    feature notice 與 alert 測試

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ModalOne from "./components/ModalOne";
+import { notification, Alert } from "antd";
 
 function App() {
-  const [openModal, setModal1Open] = useState(true);
+  const [api, contextHolder] = notification.useNotification();
+
+  console.log(api, "看看");
+
+  const alertType = ["success", "info", "warning", "error"];
+  const [isAlert, setIsAlert] = useState(false);
+
+  const [openModal, setModal1Open] = useState(false);
+  useEffect(() => {
+    api.success({
+      type: "success",
+      message: "Notification Title",
+      description: "description",
+      className: "bg-blue-400",
+    });
+  }, []);
+
   return (
     <>
+      {contextHolder}
+
+      {isAlert && <Alert message="Warning" type="warning" showIcon closable />}
+
       <h1 className="text-3xl font-bold underline"></h1>
       <ModalOne
         title={"test"}
@@ -12,7 +33,6 @@ function App() {
         cancelAction={() => setModal1Open(false)}
         okAction={() => setModal1Open(false)}
       ></ModalOne>
-
       <button onClick={() => setModal1Open(true)}>Test</button>
     </>
   );
