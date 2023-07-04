923b3178bc169be4294d1f50afad237501435f88
jim.liu <jim.liu@kangdainfo.com>
2023-07-04 16:46:33 +0800

    modify 版面變更

--- src/components/ModalTwo/index.jsx
+++ src/components/ModalTwo/index.jsx
@@ -4,7 +4,7 @@ import { CloseCircleFilled } from "@ant-design/icons";
 
 const ModalTwo = ({
   title,
-  linePay = true,
+  remind = true,
   okText = "確認，建立服務單",
   cancelText = "上一步",
   isClose = true,
@@ -15,6 +15,7 @@ const ModalTwo = ({
   const [phoneNumber, setPhoneNumber] = useState("");
   const [name, setName] = useState("");
   const [serviceType, setServiceType] = useState("");
+  const [amount, setAmount] = useState("");
 
   const showTitle = () => {
     return (
@@ -43,11 +44,7 @@ const ModalTwo = ({
           )}
         </div>
 
-        {linePay && (
-          <div className="text-[18px] text-[#275682] font-bold pt-[25px] cursor-pointer underline underline-offset-2">
-            發送 LINE PAY 付款通知
-          </div>
-        )}
+        {remind && <div className="text-[18px] font-bold pt-[25px]">提醒</div>}
       </div>
     );
   };
@@ -66,6 +63,10 @@ const ModalTwo = ({
     setServiceType(value);
   };
 
+  const amountChange = (value) => {
+    setAmount(value);
+  };
+
   return (
     <>
       <Modal
@@ -79,8 +80,8 @@ const ModalTwo = ({
         onCancel={cancelAction}
         maskClosable={false}
       >
-        <div className="flex flex-row flex-wrap items-stretch justify-start pt-[20px] pb-[24px] px-[20px]">
-          <div className="w-1/2 flex flex-col items-start justify-center">
+        <div className="flex flex-row flex-wrap items-start justify-between pt-[20px] pb-[24px] px-[20px]">
+          <div className="w-[236px] flex flex-col items-start justify-center">
             <span className="text-[16px]">客戶姓名:</span>
 
             <Input
@@ -91,16 +92,16 @@ const ModalTwo = ({
             />
           </div>
 
-          <div className="w-1/2">
-            <div className="flex flex-col items-start justify-center pl-[25px]">
+          <div className="w-[236px]">
+            <div className="flex flex-col items-start justify-center">
               <span className="text-[16px]">客戶聯絡電話:</span>
               <span className="text-[18px] font-bold pt-[8px]">0987654321</span>
             </div>
           </div>
         </div>
 
-        <div className="flex flex-row flex-wrap items-stretch justify-start px-[20px] pb-[18px]">
-          <div className="w-1/2 flex flex-col items-start justify-center">
+        <div className="flex flex-row flex-wrap items-start justify-between px-[20px] pb-[18px]">
+          <div className="w-[236px] flex flex-col items-start justify-center">
             <span className="text-[16px]">服務項目:</span>
 
             <Select
@@ -115,7 +116,22 @@ const ModalTwo = ({
             />
           </div>
 
-          <div className="w-1/2"></div>
+          <div className="w-[236px]">
+            <div className="flex flex-col items-start justify-center">
+              <span className="text-[16px]">數量:</span>
+
+              <Select
+                className="w-full text-[16px] font-bold mt-[5px] border border-solid border-[#d9d9d9] rounded-[6px] py-[5px]"
+                placeholder={<span>請使用下拉選項</span>}
+                bordered={false}
+                options={[
+                  { value: "1", label: "1" },
+                  { value: "2", label: "2" },
+                ]}
+                onChange={amountChange}
+              />
+            </div>
+          </div>
         </div>
 
         <div className="flex flex-row flex-wrap items-center justify-center px-[20px]">
@@ -123,7 +139,7 @@ const ModalTwo = ({
             <span className="text-[16px]">客戶聯絡電話:</span>
 
             <Input
-              className="text-[16px] font-bold py-[9px] mr-[25px]"
+              className="text-[16px] font-bold py-[9px] mt-[5px]"
               placeholder="請輸入電話號碼"
               allowClear
               maxLength="10"
