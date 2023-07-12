b0fc9a0d7b540e5c8acbfb40776d8ef65eaa140b
jim.liu <jim.liu@kangdainfo.com>
2023-07-12 10:57:32 +0800

    modify sheetDetailModal 版面變更

--- src/components/SheetDetailModal/index.jsx
+++ src/components/SheetDetailModal/index.jsx
@@ -1,22 +1,24 @@
-import { Modal, Button, Card } from "antd";
+import { Modal, Button, Card, Input } from "antd";
 import { CloseCircleFilled } from "@ant-design/icons";
 import closeIcon from "../../assets/images/closeIcon.svg";
 
 const SheetDetailModal = ({
   title,
   okText = "客戶 現金付款",
-  cancelText = "發送 LINE Pay付款通知",
+  cancelText = undefined,
   okAction,
   cancelAction,
-  unpaid = true,
+  unpaid = false,
   oriCharge = 100000,
-  extraCharge = false,
+  extraCharge = true,
+  payExtraCharge = false,
   linePay = 100,
   cash = 80,
   isCloseIcon = true,
   isOpen,
   remind = true,
-  messageType = "LINE訊息",
+  messageType = "payment",
+  // "LINE Pay付款訊息"
 }) => {
   const detailList = [
     { name: "服務單號", value: "9403092950" },
@@ -47,7 +49,7 @@ const SheetDetailModal = ({
     >
       <div className="text-[22px] font-bold pb-[20px]">{title}</div>
 
-      <div className="flex flex-row items-start justify-center">
+      <div className="flex flex-row items-stretch justify-center">
         <div className="w-1/2 pr-[30px]">
           {unpaid && ( //有未繳金額才會出現
             <Card
@@ -148,27 +150,39 @@ const SheetDetailModal = ({
           </Card>
         </div>
 
-        <div className="w-1/2 flex flex-row flex-wrap items-center justify-start pl-[10px]">
-          {detailList.map((item) => (
-            <div
-              key={item.name}
-              className="w-1/2 flex flex-col items-start justify-center pb-[24px]"
-            >
-              <span className="text-[16px]">{item.name}:</span>
-              <span
-                className={`text-[18px] font-bold pt-[8px] ${
-                  item.name === "訂單進度" && "text-[#275682]"
-                }`}
-              >
-                {item.value}
-              </span>
-            </div>
-          ))}
+        <div className="w-1/2 flex flex-col items-center justify-between pl-[10px]">
+          <div className="w-full flex flex-row flex-wrap items-enter justify-start">
+            {payExtraCharge ? (
+              <>
+                <span className="text-[16px]">補增金額:</span>
+                <Input
+                  className="text-[16px] border border-solid border-[#d9d9d9] rounded-[6px] py-[6px] mt-[8px]"
+                  placeholder="請輸入補增金額"
+                />
+              </>
+            ) : (
+              detailList.map((item) => (
+                <div
+                  key={item.name}
+                  className="w-1/2 flex flex-col items-start justify-center pb-[24px]"
+                >
+                  <span className="text-[16px]">{item.name}:</span>
+                  <span
+                    className={`text-[18px] font-bold pt-[8px] ${
+                      item.name === "訂單進度" && "text-[#275682]"
+                    }`}
+                  >
+                    {item.value}
+                  </span>
+                </div>
+              ))
+            )}
+          </div>
 
-          <div className="flex flex-col items-center justify-center pt-[16px] pb-[20px]">
-            <div className="flex flex-row items-center justify-center">
-              {cancelAction && (
-                <div className="w-[200px] mx-3">
+          <div className="w-full flex flex-col items-center justify-center pt-[16px] pb-[20px]">
+            <div className="w-full flex flex-row items-center justify-center">
+              {cancelText && (
+                <div className="w-[200px] mx-2">
                   <Button
                     block
                     ghost
@@ -180,8 +194,8 @@ const SheetDetailModal = ({
                   </Button>
                 </div>
               )}
-              {okAction && (
-                <div className="w-[200px] mx-3">
+              {okText && (
+                <div className="w-[200px] mx-2">
                   <Button
                     block
                     type="primary"
@@ -196,9 +210,20 @@ const SheetDetailModal = ({
 
             {remind && (
               <div className="text-[14px] pt-[12px]">
-                客戶完成取件時，系統將發送
-                <span className="font-bold px-1">{messageType}</span>
-                通知客戶物件已被領取
+                {messageType === "message" && (
+                  <div>
+                    客戶完成取件時，系統將發送
+                    <span className="font-bold px-1">LINE訊息</span>
+                    通知客戶物件已被領取
+                  </div>
+                )}
+                {messageType === "payment" && (
+                  <div>
+                    確認後，系統將發送
+                    <span className="font-bold px-1">LINE Pay付款訊息</span>
+                    通知該客戶前往付款
+                  </div>
+                )}
               </div>
             )}
           </div>
