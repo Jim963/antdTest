31fb75aac565649452128ae1bd144d0d88825a29
jim.liu <jim.liu@kangdainfo.com>
2023-07-18 10:18:26 +0800

    modify SideBar 改ts

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,83 @@
+import { Button } from "antd";
+import LogoImg from "../../assets/images/logo.png";
+import goIcon from "../../assets/images/goIcon.svg";
+import searchIcon from "../../assets/images/searchIcon.svg";
+import tableIcon from "../../assets/images/tableIcon.svg";
+import sheetIcon from "../../assets/images/sheetIcon.svg";
+import orangeGoICon from "../../assets/images/orangeGoIcon.svg";
+
+interface RouteItem {
+  name: string;
+  icon: string;
+  route: string;
+}
+
+const SideBar = () => {
+  const pageList: RouteItem[] = [
+    { name: "查詢門市/服務單", icon: searchIcon, route: "" },
+    { name: "門市/會員資訊", icon: sheetIcon, route: "" },
+    { name: "營業/會員報表", icon: tableIcon, route: "" },
+  ];
+
+  const pageList_admin: RouteItem[] = [
+    { name: "查詢服務單", icon: searchIcon, route: "" },
+    { name: "營業報表", icon: tableIcon, route: "" },
+  ];
+
+  const newStore = () => {
+    console.log("build new store");
+  };
+  return (
+    <div className="relative w-[180px] xl:w-[240px] max-h-[1200px]">
+      <div className="flex flex-col items-start justify-start px-[24px] pt-[54px]">
+        <img
+          className="w-[120px] xl:w-[180px] h-[38px] xl:h-[56px] mb-[50px]"
+          src={LogoImg}
+        />
+        <div className="box-border w-[130px] xl:w-[190px] h-[80px] bg-[#FFF2E7] rounded-[8px] py-[12px] px-[16px]">
+          <div className="flex flex-row items-center justify-between">
+            <div className="flex flex-col items-center justify-start">
+              <span className="text-[14px]">未收件服務單:</span>
+              <div className="flex flex-row items-center">
+                <span className="text-[28px] text-[#F87700] font-bold">32</span>
+                <span className="ml-[8px] text-[16px]">件</span>
+              </div>
+            </div>
+
+            <img src={orangeGoICon} />
+          </div>
+        </div>
+        <div className="h-[1px] w-full my-[24px] bg-[#D9D9D9]"></div>
+
+        {pageList.map((item) => {
+          return (
+            <div
+              key={item.name}
+              className="w-full flex flex-row items-center justify-between text-[16px] mb-[20px]"
+            >
+              <div className="flex flex-row items-center justify-center">
+                <img className="mr-[4px]" src={item.icon} alt="" />
+                <span> {item.name}</span>
+              </div>
+              <img src={goIcon} />
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="absolute bottom-[40px] w-[160px] xl:w-[200px] mx-3">
+        <Button
+          block
+          ghost
+          className={"h-[48px]"}
+          type="primary"
+          onClick={newStore}
+        >
+          <span className="allCenter text-[16px]">建立新店家</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
