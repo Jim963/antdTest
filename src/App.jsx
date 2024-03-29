5d73daf1f06073f008a6e46b744f0ba119e0a61d
jim.liu <jim.liu@kangdainfo.com>
2024-03-29 17:55:51 +0800

    feat: add nature test

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useRef } from "react";
 import { useRoutes } from "react-router-dom";
 import routes from "./router/index";
 import { getUserToken } from "./utils/handleUserToken";
@@ -5,16 +6,43 @@ import { getUserToken } from "./utils/handleUserToken";
 //test
 import SignBox from "./components/SignTest";
 
+// nature
+import NatureSignTest from "./components/NatureSignTest";
+
 const App = () => {
   const userToken = getUserToken();
   console.log(userToken, "userToken");
   const routeElement = useRoutes(routes(userToken));
 
+  // nature
+  const [state, setStateData] = useState({
+    tbs: "TBS", //隨機值,get from api,
+    b64Signature: "",
+    b64Cert: "",
+    returnCode: "",
+    tbsEncoding: "NONE",
+    hashAlgorithm: "SHA256",
+  });
+  const [natureTrigger, setNatureTrigger] = useState(false);
+
+  useEffect(() => {
+    console.log(state, "state change");
+  }, [state.pinCode]);
   return (
     <>
       <div className="App w-screen min-h-screen text-[#484848]">
         {/* {routeElement} */}
-        <SignBox></SignBox>
+        {/* <SignBox></SignBox> */}
+        <NatureSignTest
+          state={state}
+          setStateData={setStateData}
+          natureTrigger={natureTrigger}
+          setNatureTrigger={setNatureTrigger}
+          isPinError={false}
+          getErrorMsg={() => {
+            console.log("yap");
+          }}
+        ></NatureSignTest>
       </div>
     </>
   );
---
+++ src/components/NatureSignTest/index.jsx
@@ -0,0 +1,226 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const NatureSignTest = ({
+  state,
+  setStateData,
+  isPinError,
+  getErrorMsg,
+  natureTrigger,
+  setNatureTrigger,
+}) => {
+  const postTarget = useRef(null);
+  const [pinCode, setPinCode] = useState("");
+  const [natureState, setNatureState] = useState({
+    tbs: "TBS", //隨機值,get from api,
+    b64Signature: "",
+    b64Cert: "",
+    returnCode: "",
+    tbsEncoding: "NONE",
+    hashAlgorithm: "SHA256",
+  });
+  let timeoutId;
+
+  const login = () => {
+    console.log(natureState, "login@@@@@@@@@@@@");
+  };
+
+  // get random tbs
+  //   const AjaxGetToken = async () => {
+  //     try {
+  //       await axios({
+  //         method: "GET",
+  //         url: "gettoken",
+  //       })
+  //         .then(function (response) {
+  //           console.log(response, "AjaxGetToken");
+  //           return response;
+  //         })
+  //         .catch(function (err) {
+  //           console.log(err, "error");
+  //         });
+  //     } catch (e) {
+  //       console.log(e);
+  //       alert("系統發生錯誤！");
+  //     }
+  //   };
+
+  // 檢查簽章是否完成
+  const checkFinish = async () => {
+    if (postTarget.current) {
+      postTarget.current.close();
+      alert("尚未安裝元件");
+    }
+  };
+
+  //進行自然人簽章
+  const makeSignature = () => {
+    console.log("makeSignature", natureState);
+    console.log("makeSignature", pinCode);
+    // const ua = window.navigator.userAgent;
+    postTarget.current = window.open(
+      "http://localhost:61161/popupForm",
+      "簽章中",
+      "height=200, width=200, left=100, top=20"
+    );
+    timeoutId = setTimeout(checkFinish, 3500);
+  };
+
+  // 拿取tbs封包
+  const getTbsPackage = () => {
+    console.log("getTbsPackage", {
+      tbs: natureState.tbs,
+      tbsEncoding: natureState.tbsEncoding,
+      hashAlgorithm: natureState.hashAlgorithm,
+      pin: pinCode,
+      func: "MakeSignature",
+      signatureType: "PKCS1",
+    });
+    const tbsData = {
+      tbs: encodeURIComponent(natureState.tbs),
+      tbsEncoding: natureState.tbsEncoding,
+      hashAlgorithm: natureState.hashAlgorithm,
+      pin: pinCode,
+      func: "MakeSignature",
+      signatureType: "PKCS1",
+    };
+    return JSON.stringify(tbsData);
+  };
+
+  //賦值
+  const setSignature = (signature) => {
+    const ret = JSON.parse(signature);
+    console.log(ret, "setSignature");
+    console.log("setSignature", {
+      b64Signature: ret.signature,
+      b64Cert: ret.certb64,
+      returnCode: ret.ret_code,
+    });
+    setNatureState((pre) => {
+      return {
+        ...pre,
+        b64Signature: ret.signature,
+        b64Cert: ret.certb64,
+        returnCode: ret.ret_code,
+      };
+    });
+    if (ret.ret_code !== 0) {
+      console.log("nope");
+      //   alert(MajorErrorReason(ret.ret_code));
+      //   if (ret.last_error) alert(MinorErrorReason(ret.last_error));
+    } else {
+      postTarget.current = null;
+      login();
+    }
+    // else prepareFormData();
+  };
+
+  // 收到訊息處理
+  const receiveMessage = (event) => {
+    if (console) console.debug(event);
+
+    //安全起見，這邊應填入網站位址檢查
+    if (event.origin !== "http://localhost:61161") return;
+    try {
+      console.log("receiveMessage", pinCode, "pinCode");
+      const ret = JSON.parse(event.data);
+      if (ret.func) {
+        if (ret.func === "getTbs") {
+          clearTimeout(timeoutId);
+          const json = getTbsPackage();
+          postTarget.current.postMessage(json, "*");
+        } else if (ret.func === "sign") {
+          console.log(natureState, "nowState");
+          setSignature(event.data);
+        }
+      } else {
+        if (console) console.error("no func");
+      }
+    } catch (e) {
+      //errorhandle
+      if (console) console.error(e);
+    }
+  };
+
+  //加密
+  const encrypt = () => {
+    if (state.staffID === "") {
+      alert("請輸入帳號");
+      return;
+    }
+    if (pinCode === "") {
+      alert("請輸入卡片PIN碼");
+      return;
+    }
+    makeSignature();
+  };
+
+  React.useEffect(() => {
+    // const randomToken = AjaxGetToken();
+    // console.log(randomToken.data, "randomToken");
+    // setNatureState((pre) => ({ ...pre, tbs: randomToken.data }));
+    window.addEventListener("message", receiveMessage, false);
+
+    return () => {
+      console.log("remove!");
+      window.removeEventListener("message", receiveMessage, false);
+    };
+  });
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setNatureState((pre) => {
+      return { ...pre, [name]: value };
+    });
+  };
+
+  useEffect(() => {
+    if (!!natureTrigger) {
+      encrypt();
+      setNatureTrigger(false);
+    }
+  }, [natureTrigger]);
+
+  return (
+    <div>
+      <button onClick={() => makeSignature()}>Test</button>
+      <input
+        id="pinCode"
+        name="pinCode"
+        onChange={(e) => {
+          const { value } = e.target;
+          setPinCode(value);
+        }}
+        value={pinCode ?? ""}
+      />
+      <input
+        name="tbs"
+        id="tbs"
+        onChange={(e) => handleChange(e)}
+        value={natureState.tbs ?? ""}
+      />
+
+      <input
+        name="b64Signature"
+        id="b64Signature"
+        onChange={(e) => handleChange(e)}
+        value={natureState.b64Signature ?? ""}
+      />
+      <input
+        name="b64Cert"
+        id="b64Cert"
+        onChange={(e) => handleChange(e)}
+        value={natureState.b64Cert ?? ""}
+      />
+      <input
+        name="returnCode"
+        id="returnCode"
+        onChange={(e) => handleChange(e)}
+        value={natureState.returnCode ?? ""}
+      />
+      <input name="tbsEncoding" id="tbsEncoding" defaultValue="NONE" />
+      <input name="hashAlgorithm" id="hashAlgorithm" defaultValue="SHA256" />
+    </div>
+  );
+};
+
+export default NatureSignTest;
